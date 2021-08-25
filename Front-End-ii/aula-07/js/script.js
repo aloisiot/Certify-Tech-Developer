42ac125d520ce4c49ddcbd43edadd269115bbd34
aloisiot <loistc.carvalho@gmail.com>
2021-08-25 16:59:30 -0300

    Aula 7 - eventListener

--- Front-End-ii/aula-07/js/script.js
+++ Front-End-ii/aula-07/js/script.js
@@ -1,32 +1,58 @@
+window.onload = alert('Bem vindo');
 
+const body = document.querySelector('body');
 
-function injectForm(){
-    const body = document.querySelector('body');
-    
-    const form = document.createElement('form');
-    body.appendChild(form);
-    
-    const h1 = document.createElement('h1');
-    form.appendChild(h1);
-    h1.innerHTML='Login';
-    
-    const inputEmail = document.createElement('input');
-    inputEmail.setAttribute('placeholder', 'email');
-    form.appendChild(inputEmail);
-    
-    const inputPassword = document.createElement('input');
-    inputPassword.setAttribute('placeholder', 'senha');
-    form.appendChild(inputPassword);
-    
-    const submitButton = document.createElement('button');
-    submitButton.innerHTML= 'Enviar';
-    submitButton.setAttribute('type','submit');
-    form.appendChild(submitButton);
-    
-    const resetButton = document.createElement('button');
-    resetButton.innerHTML= 'Calcelar'; 
-    resetButton.setAttribute('type','submit');
-    form.appendChild(resetButton);
-}
+const form = document.createElement('form');
+body.appendChild(form);
 
-window.addEventListener("load", injectForm);
+const h1 = document.createElement('h1');
+form.appendChild(h1);
+h1.innerHTML='Login';
+
+const inputEmail = document.createElement('input');
+inputEmail.setAttribute('placeholder', 'email');
+inputEmail.type='email';
+form.appendChild(inputEmail);
+
+const inputPassword = document.createElement('input');
+inputPassword.setAttribute('placeholder', 'senha');
+inputPassword.type='password';
+inputPassword.id = 'pass'
+form.appendChild(inputPassword);
+
+const submitButton = document.createElement('button');
+submitButton.innerHTML= 'Enviar';
+submitButton.type='submit';
+form.appendChild(submitButton);
+
+const resetButton = document.createElement('button');
+resetButton.innerHTML= 'Cancelar'; 
+resetButton.id='reset-button';
+resetButton.type='reset';
+form.appendChild(resetButton);
+
+// Elimina o comportamento padrão do botão reset depois de clicado a primeira vez.
+resetButton.addEventListener('click', function(event){
+    event.preventDefault();
+});
+
+// Altera a cor do titulo quando o mouse é posicionado sobre dele
+h1.addEventListener('mouseenter',()=>{
+    h1.style.color='red';
+})
+
+// Altera a cor do titulo quando o mouse é retirado de cima dele
+h1.addEventListener('mouseout',()=>{
+    h1.style.color='white';
+})
+
+/* Para cada caracter inserido no input senha
+   insere um paragrafo ao final do form */
+let count = 0
+inputPassword.addEventListener('keydown', function(event){
+    count++;
+    let result = `Ultima tecla precionada: ${event.key}, repetições: ${count}`
+    p = document.createElement('p');
+    p.innerHTML=result;
+    form.appendChild(p);
+})
