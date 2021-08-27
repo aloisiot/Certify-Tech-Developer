aaa15cd34cb0d9090e616d74cedb195b690de105
aloisiot <loistc.carvalho@gmail.com>
2021-08-27 00:09:24 -0300

    estilizando msg de erro e comentando o codigo

--- Front-End-ii/aula-09/js/script.js
+++ Front-End-ii/aula-09/js/script.js
@@ -19,7 +19,10 @@ const inputPassword = document.createElement('input');
 inputPassword.setAttribute('placeholder', 'senha');
 inputPassword.type='password';
 inputPassword.id = 'pass'
-form.appendChild(inputPassword);
+
+const countainerImputPassword = document.createElement("div");
+countainerImputPassword.appendChild(inputPassword);
+form.appendChild(countainerImputPassword);
 
 const submitButton = document.createElement('button');
 submitButton.innerHTML= 'Enviar';
@@ -33,26 +36,31 @@ resetButton.type='reset';
 form.appendChild(resetButton);
 
 
+// Desabilita o input EMAIL com a saida do mouse
 inputEmail.addEventListener('mouseout',()=>{
     if(inputEmail.value!=''){
         inputEmail.disabled = true;
     }
 })
 
+// Desabilita o input PASSWORD com a saida do mouse
 inputPassword.addEventListener('mouseout',()=>{
     if(inputPassword.value!=''){
         inputPassword.disabled = true;
     }
 })
 
-countainerImputEmail.addEventListener('mouseenter',()=>{
+// Reabilita o input PASSWORD com a entrada do mouse no elemento pai
+countainerImputPassword.addEventListener('mouseenter',()=>{
     inputPassword.disabled=false;
 })
 
+// Reabilita o input EMAIL com a entrada do mouse no elemento pai
 countainerImputEmail.addEventListener('mouseenter',()=>{
         inputEmail.disabled=false;
 })
 
+// Exibe uma mensagem de erro caso algum input esteja vazio durante a submição formulario
 submitButton.addEventListener('click',(event)=>{
     
     event.preventDefault();
@@ -60,9 +68,10 @@ submitButton.addEventListener('click',(event)=>{
     const p0 = document.querySelector('#is-empity-alert');
 
     let p = document.createElement('p');
-    p.id='alert-is-empity'
+    p.id='is-empity-alert'
     p.innerHTML= 'Preencha os campos para continuar'
     p.style.color='red';
+    p.style.fontSize = "0.8rem"
 
     try{
         form.removeChild(p0);
