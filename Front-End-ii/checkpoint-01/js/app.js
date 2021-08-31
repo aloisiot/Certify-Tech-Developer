af9e9ed48af5623ebc8e5461f79a1e74d2ab497a
aloisiot <loistc.carvalho@gmail.com>
2021-08-31 18:33:20 -0300

    desafio front checkpoint-01 primeiro commit

---
+++ Front-End-ii/checkpoint-01/js/app.js
@@ -0,0 +1,13 @@
+document.getElementById("submit-button").addEventListener( 'click',
+    (event) => {
+        
+        event.preventDefault();
+
+        let url = document.getElementById("url-input").value;
+
+        if(url != ""){
+            document.body.style.backgroundImage = `url('${url}')`;
+        }
+
+    }
+)
