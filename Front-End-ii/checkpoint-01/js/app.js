8d6499911fdee26399bc3d07b39410e5dc3e951a
aloisiot <loistc.carvalho@gmail.com>
2021-09-01 16:02:43 -0300

    identação

--- Front-End-ii/checkpoint-01/js/app.js
+++ Front-End-ii/checkpoint-01/js/app.js
@@ -1,13 +1,12 @@
 document.getElementById("submit-button").addEventListener( 'click',
     (event) => {
-        
+
         event.preventDefault();
 
         let url = document.getElementById("url-input").value;
 
         if(url != ""){
             document.body.style.backgroundImage = `url('${url}')`;
-        }
-
     }
-)
+
+})
