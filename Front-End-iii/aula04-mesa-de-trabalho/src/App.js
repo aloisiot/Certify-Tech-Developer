a720d0eda5fc14a2312bdb0d344f0168557fccf9
aloisiot <aloisiot.c98@gmail.com>
2021-10-26 09:58:54 -0300

    Aula 04 - Mesa de trabalho

---
+++ Front-End-iii/aula04-mesa-de-trabalho/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import homemFeliz from './homem-feliz-dinheiro.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={homemFeliz}></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
